e3d93fffc5e7d6196718f7c2242e0a3fae20edf4
Shane Rees <rees.shane@gmail.com>
2019-10-01 20:08:23 +0930

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env-python3
+
+
+print('Welcome to a hello word for python.')
+name = input("Please enter you name: ")
+print("Hi " + name + " welcome to python")
+print("python is very powerful")
