8a8f07951a89421362b5c8578fe4c46db5adc0b9
Marcos Vieira <mavbvieira@gmail.com>
2019-10-01 12:11:48 -0300

    add hello_worls in python

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+name = ''
+
+while(name != 'x'):
+  print('Hello World')
+  name = raw_input('Please enter your name or x to quit ')
+  print('Welcome, {nome}').format(nome = name)
+else:
+  print('bye')
