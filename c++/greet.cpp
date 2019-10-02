532c7f5076d222018e5b6591fd501d8b10f14656
Soham Chakrabarti <soham17041998@gmail.com>
2019-10-02 11:56:54 +0530

    Update greet.cpp

--- c++/greet.cpp
+++ c++/greet.cpp
@@ -1,12 +1,59 @@
-#include<iostream>
-using namespace std;
+#include<bits/stdc++.h>
+ 
+using namespace std; 
+#define FR(i,j,k) for(int i=j;i<k;++i)
 
-int main()
+void solve(vector<string> &names)
 {
-	char user[20];
+	for(string name : names)std::cout << name << ' ';
+}
 
-    cout<<"Enter Your Name!\n";
-	cin>>user;
-	cout<<"Welcome "<< user <<"\n";
+void caller()
+{
+	cout<<"Enter Your Name! \n";
+}
+
+string getLine()
+{
+	string data;
+	getline(std::cin,data);data+=" ";
+	return data;
+}
+
+vector<string> process(string data)
+{
+	vector<string> user;
+	string temp_string="";
+
+	FR(i,0,data.length())
+    {
+    	if(data[i]==' '){
+    		user.push_back(temp_string);
+    		temp_string="";
+    	} 
+    	else 
+    		temp_string += data[i];
+    }	
+    return user;
+}
+int32_t main()
+{
+
+	//call my caller()
+	caller(); 
+	
+	//get user name
+    string data = getLine();
+
+    //spliting the string
+    vector<string> splitName = process(data);
+
+	std::cout << "Welcome ";
+
+	solve(splitName);
+
+	std::cout << "\n";
     return 0;
 }
+
+//Too Complicated ?
