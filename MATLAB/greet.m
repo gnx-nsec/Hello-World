9fd294549ccb83e7461e8990b78394f30bb98777
Sparsh Agarwal <sparsh21agarwal@gmail.com>
2019-10-06 01:43:42 +0530

    Add hello world samples for Scala

--- MATLAB/greet.m
+++
@@ -1,2 +0,0 @@
-name = input(' ');
-fprintf('Welcome, %s.\n',name);
--- MATLAB/pattern.m
+++
@@ -1,8 +0,0 @@
-for i=1:4
-    string = '     '; 
-    index1 = 1;
-    index2 = i;
-    string(index1:index2) = '*';
-    fprintf('%s\n', string);   
-end
- 
